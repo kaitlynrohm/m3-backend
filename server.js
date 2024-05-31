b8da353da663d2330d9083755d3512fa6a8ce40b
Kaitlyn Rohm <kaitlyn.j.rohm@gmail.com>
2024-05-31 14:56:02 +1200

    Initial set up

---
+++ server.js
@@ -0,0 +1,37 @@
+// ====== Packages and imports ====== //
+const express = require("express");
+const cors = require("cors"); // Import cors middleware once
+const app = express();
+require("dotenv").config();
+
+//Import routes
+const chatBox = require("./routes/chatBox.js");
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// =========== ENDPOINTS =========== //
+// Initial setup in Postman
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// chat box endpoint
+app.use(chatBox);
+
+// ============== PORT ============== //
+const PORT = 3000 || process.env.PORT;
+app
+  .listen(PORT, () => {
+    console.log(`Server is alive on http://localhost:${PORT}`);
+  })
+  .on("error", (error) => {
+    if (error.code === "EADDRINUSE") {
+      console.log("PORT is already in use.");
+    } else {
+      console.log("Server Errors: ", error);
+    }
+  });
+
+module.exports = app;
