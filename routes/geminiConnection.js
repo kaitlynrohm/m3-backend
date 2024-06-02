60d02802f7fc09688fca5a848816c845f7e3b334
Kaitlyn Rohm <kaitlyn.j.rohm@gmail.com>
2024-06-02 14:10:18 +1200

    Added system instructions to gemini connection. Main api done.

--- routes/geminiConnection.js
+++ routes/geminiConnection.js
@@ -10,15 +10,30 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 // API key
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
-// The Gemini model
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
-
 router.post("/api/gemini-connection", async (req, res) => {
-  const chat = model.startChat({
-    history: req.body.history,
+  const title = req.body.title;
+  let message;
+  req.body.message ? (message = req.body.message) : (message = "begin");
+
+  // The Gemini model
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+    systemInstruction: `Turners Car Auctions is a market leader in the automotive industry in New Zealand, responsible for more than 10% of all used vehicle sales in the country.
+    Turners is currently undertaking a major re-design of its motor vehicle insurance systems and processes, including an investigation of ways in which the introduction of new technologies can enhance the buyer experience.
+    Turners Car Insurance is an internal division of Turners Car Auctions.
+    Due to restructuring a lot of staff will need to be re-trained into different rolls within the company.
+    You are Zane, an executive at Turners Car Insurance.
+    You are interviewing an employee for a ${title} role.
+    Start by asking, "Tell me about yourself." Then ask at least 6 questions relevant to the role. Adjust your questions based on the user's responses`,
   });
 
-  const message = req.body.message;
+  let chat;
+
+  if (req.body.history) {
+    chat = model.startChat({ history: req.body.history });
+  } else {
+    chat = model.startChat();
+  }
 
   const result = await chat.sendMessage(message);
   const response = await result.response;
